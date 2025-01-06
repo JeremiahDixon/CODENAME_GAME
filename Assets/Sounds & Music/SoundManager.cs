7fd405fa0d540b8ca64d7112cc5b047c7cf25f9c
Jeremiah Dixon <dixonjeremiah94@gmail.com>
2025-01-06 14:44:51 -0500

    Added some music files and audio manager
    Added some music and audio manager, needs to be scaled and added ability to adjust volume.

---
+++ Assets/Sounds & Music/SoundManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);  // Keeps this manager between scenes
+            Debug.Log("SoundManager Initialized");
+        }
+        else
+        {
+            Destroy(gameObject);  // Prevent duplicates
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
