6feca57b27817e85b27a9af2f35dc7d091e7363f
Jeremiah Dixon <dixonjeremiah94@gmail.com>
2025-01-06 10:47:50 -0500

    Main Menu Changes & enemy rb
    Added colors to selecting and highlighting buttons etc, and changed all enemy rb to kinematic so they go through terrain for now until I create some path finding system.

--- Assets/Scenes/MianMenu/MainMenuScript.cs
+++ Assets/Scenes/MianMenu/MainMenuScript.cs
@@ -1,7 +1,9 @@
+using UnityEditor.MPE;
 using UnityEngine;
 
 public class MainMenuScript : MonoBehaviour
 {
+    bool classSelected = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,10 +11,15 @@ public class MainMenuScript : MonoBehaviour
     }
 
     public void LoadPlayScene(){
-        GameManager.Instance.LoadAScene("VS");
+        if(classSelected){
+            GameManager.Instance.LoadAScene("VS");
+        }else{
+            Debug.Log("You gotta select a class man");
+        }
     }
 
     public void SelectClass(string className){
         GameManager.Instance.setPlayerClassSo(className);
+        classSelected = true;
     }
 }
